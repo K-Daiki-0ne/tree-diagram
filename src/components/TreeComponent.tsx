06ea2e69462b0bebca498edb6225dcfd6727c6b9
_ 0ne <kouguchi3456@yahoo.co.jp>
2021-08-16 06:38:57 +0900

    create: custom desing & base data

--- src/components/TreeComponent.tsx
+++ src/components/TreeComponent.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import Tree from 'react-tree-graph';
 import { data } from '../data/tree_data';
-import 'react-tree-graph/dist/style.css'
+import 'react-tree-graph/dist/style.css';
+import './TreeComponent.css'
 
 export const TreeComponent: React.FC = (): JSX.Element => {
   return (
-    <div>
+    <div className='tree-view'>
       <Tree 
         data={data}
         animated
-        height={200}
-        width={400}
+        height={800}
+        width={1000}
+        svgProps={{
+          className: 'custom'
+        }}
       />
     </div>
   )
--- src/data/tree_data.ts
+++ src/data/tree_data.ts
@@ -4,8 +4,28 @@
 export const data = {
 	name: 'Parent',
 	children: [{
-		name: 'Child One'
+		name: 'Child One',
+    children: [{
+      name: 'Child Theree',
+    }, {
+      name: 'Child Four'
+    }, {
+      name: 'Child Five'
+    }]
 	}, {
-		name: 'Child Two'
+		name: 'Child Two',
+    children: [{
+      name: 'Child Six',
+      children: [{
+        name: 'Child Seven'
+      }, {
+        name: 'Child Eight'
+      }, {
+        name: 'Child Nine',
+        children: [{
+          name: 'Child Ten'
+        }]
+      }]
+    }]
 	}]
 };
